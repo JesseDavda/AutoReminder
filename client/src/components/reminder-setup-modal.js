05bb989f47b46eb2819ec8832052d1dc510252e3
JesseDavda <jessedavda@gmail.com>
2019-01-06 21:41:54 +0000

    Starts work on the backend API

--- client/src/components/reminder-setup-modal.js
+++ client/src/components/reminder-setup-modal.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 //stylesheet import
 import '../assets/stylesheets/new-reminder-modal.css';
 
 export default class ReminderModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleCreateClick = this.handleCreateClick.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleTimeChange = this.handleTimeChange.bind(this);
+
+        this.state ={
+            reminder_name: "",
+            reminder_date: "",
+            reminder_time: "",
+        }
+    }
+
+    handleCreateClick() {
+        let postBody = {
+            reminder_name: this.reminder_name,
+            reminder_date: this.reminder_date,
+            reminder_time: this.reminder_time
+        }
+
+        axios.post('http://localhost:8000/newReminder', postBody)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(e => {
+                console.log("Error: ", e);
+            });
+    }
+
+    handleNameChange(data) {
+        this.setState({
+            reminder_name: data
+        })
+    }
+
+    handleDateChange(data) {
+        this.setState({
+            reminder_date: data
+        })
+    }
+
+    handleTimeChange(data) {
+        this.setState({
+            reminder_time: data
+        })
+    }
+
     render() {
         return(
             <div className="reminder-modal-container">
@@ -12,15 +62,15 @@ export default class ReminderModal extends Component {
                     <div className="inputs-container">
                         <div className="reminder-setup input">
                             <label for="reminder_name"><p>Name:</p></label>
-                            <input type="text" />
+                            <input type="text" onChange={handleNameChange}/>
                         </div>
                         <div className="reminder-setup date">
                             <label for="reminder_date"><p>Date:</p></label>
-                            <input type="date" />
+                            <input type="date"  onChange={handleDateChange}/>
                         </div>
                         <div className="reminder-setup time">
                             <label for="reminder_time"><p>Time</p></label>
-                            <input type="time" />
+                            <input type="time" onChange={handleTimeChange}/>
                         </div>
                         <div className="create-reminder-button"><p>Create Reminder!</p></div>
                     </div>
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-const port = process.env.PORT || 3000
+router.post('/newReminder', (ctx, next) => {
+    console.log(ctx);
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const port = process.env.PORT || 8000
 app.listen(port);
+console.log('Server listening on port: ', port);
